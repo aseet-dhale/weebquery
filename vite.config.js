8f5b6487b7f0cfe7ea5a3a8d06e5c2c22b6f2254
Aseet Dhale <aseetdhale10800@gmail.com>
2025-07-03 01:35:10 +0530

    Fix GH page deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
-    base: "weebquery",
+    base: "/weebquery",
     build: {
       outDir: "build",
     },
